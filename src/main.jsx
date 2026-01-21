269deecb2bc9cb5016ff68ed97067058f54637ae
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-01-21 21:39:33 +0600

    added more

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
